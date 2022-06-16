94d852afa3937b8829cf2d8cc7dde3953cdbf4bc
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2022-06-16 22:06:08 +0600

    UI-started

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../../shared/Navigation";
+import Services from "../Services/Services";
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation></Navigation>
+      <Services></Services>
+    </div>
+  );
+};
+
+export default Home;
