01931ce212a31ab17110e4372c91ea713aa3391e
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2022-06-24 12:18:17 +0600

    updated

--- src/pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -3,13 +3,13 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import doctor from "../../../images/doctor.png";
 import bg from "../../../images/appointment.png";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 const apointBg = {
   background: `url(${bg})`,
 };
 const AppointmentBanner = () => {
   return (
-    <Box style={apointBg} sx={{ flexGrow: 1, marginTop: "200px" }}>
+    <Box style={apointBg} sx={{ flexGrow: 1, marginY: "200px" }}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <img
@@ -19,7 +19,34 @@ const AppointmentBanner = () => {
           />
         </Grid>
         <Grid item xs={12} md={6}>
-          <Typography>desct</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              flexDirection: "column",
+              textAlign: "left",
+            }}
+          >
+            <Typography
+              variant="h5"
+              style={{ color: "#4CE6E6" }}
+              sx={{ my: 4 }}
+            >
+              appointment
+            </Typography>
+            <Typography
+              variant="h4"
+              style={{ color: "white", fontWeight: 500 }}
+            >
+              Make an appointment Today{" "}
+            </Typography>
+            <Typography variant="h6" style={{ color: "white" }} sx={{ my: 4 }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's
+            </Typography>
+            <Button variant="contained">Contained</Button>
+          </Box>
         </Grid>
       </Grid>
     </Box>
